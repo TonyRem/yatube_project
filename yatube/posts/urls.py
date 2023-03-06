c53998cdb73b58f687ad414e2f7c7804e91e1c4f
Anton Remizov <remizov.avas@gmail.com>
2023-03-06 21:46:52 +0400

    Add database

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,10 +3,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     # Home page
-    path('', views.index),
+    path('', views.index, name='home'),
     # Groups pages
-    path('group/<slug:slug>/', views.group_posts)
+    path('group_list.html/', views.group_posts, name='group_list')
 
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
+
 
 # Перевести после проверки
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, template, context)
 
 
-def group_posts(request, slug):
-    return HttpResponse('Группа {}.'.format(slug))
+def group_posts(request):
+    template = "posts/group_list.html"
+    title = 'Лев Толстой – зеркало русской революции.'
+    context = {
+        'title': title,
+        'text': "Здесь будет информация о группах проекта Yatube",
+    }
+    return render(request, template, context)
