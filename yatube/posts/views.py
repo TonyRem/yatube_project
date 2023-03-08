0e894f80dd839c668b240eda704cfb3866390609
Anton Remizov <remizov.avas@gmail.com>
2023-03-08 13:33:19 +0400

    Update templates

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
+POSTS_PER_PAGE = 10
+
 
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.all()[:POSTS_PER_PAGE]
     context = {
         'text': 'Последние обновления на сайте Yatube',
         'posts': posts,
@@ -15,7 +17,7 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = group.posts.all()[:POSTS_PER_PAGE]
     context = {
         'group': group,
         'posts': posts,
